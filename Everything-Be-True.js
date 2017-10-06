b82b6b055691f997b6fe943bde108925b594fd6e
Mario Cueva <mario.cueva.au@gmail.com>
2017-10-06 23:33:23 +1100

    shortened the code

--- Everything-Be-True.js
+++ Everything-Be-True.js
@@ -8,20 +8,9 @@ function truthCheck(collection, pre) {
   var result = collection.map((value) => {
 
     if (value.hasOwnProperty(pre)) {
-
       if (typeof value[pre] === "boolean") return true;
-      if ((typeof value[pre] === "number") && value[pre] > 0) return true;
       if ((typeof value[pre] === "string") && value[pre] !== "") return true;
-      if (isNaN(value[pre])) return false;
-
-      switch (value[pre]) {
-        case null:
-          return false;
-        case undefined:
-          return false;
-        default:
-          return false;
-      }
+      return false;
     } else {
       return false;
     }
@@ -31,20 +20,20 @@ function truthCheck(collection, pre) {
   return result.find((anyValue) => anyValue === false) !== false;
 }
 
-console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")); // should return true.
+console.log("true: " + truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")); // should return true.
 
-  console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")); // should return false.
+  console.log("false: " + truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")); // should return false.
 
-  console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age")); // should return false.
+  console.log("false: " + truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age")); // should return false.
 
-  console.log(truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat")); // should return false
+  console.log("false: " + truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat")); // should return false
 
-  console.log(truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat")); // should return true
+  console.log("true: " + truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat")); // should return true
 
-  console.log(truthCheck([{"single": "yes"}], "single")); // should return true
+  console.log("true: " + truthCheck([{"single": "yes"}], "single")); // should return true
 
-  console.log(truthCheck([{"single": ""}, {"single": "double"}], "single")); // should return false
+  console.log("false: " + truthCheck([{"single": ""}, {"single": "double"}], "single")); // should return false
 
-  console.log(truthCheck([{"single": "double"}, {"single": undefined}], "single")); // should return false
+  console.log("false: " + truthCheck([{"single": "double"}, {"single": undefined}], "single")); // should return false
 
-  console.log(truthCheck([{"single": "double"}, {"single": NaN}], "single")); // should return false
+  console.log("false: " + truthCheck([{"single": "double"}, {"single": NaN}], "single")); // should return false
